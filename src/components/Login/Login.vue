2198ceacf449f7a420263130bd93b982ab210e22
emrys <1030882725@qq.com>
2018-07-14 15:45:32 +0800

    login
    Change-Id: Ia602c40cf387be0e333a6b1c49de2e1d2f574e8d

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,69 +1,71 @@
 <template>
   <div class="login-container">
-      <div class="loginbox" v-if="loginStatus">
-         <el-form  id="login" class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" status-icon label-width="80px" ref="loginForm" label-position="left">
-          <h3 class="title">vue-element-admin</h3>
-          <el-form-item label="账号" prop="username">
-            <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-            <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"></el-input>
-          </el-form-item>
-          <el-form-item class="el-form-item-button">
-            <el-button type="primary" @click.native.prevent="handleLogin" :loading="loading">立即登录</el-button>
-            <el-button type="primary" @click="switchPages()">注册界面</el-button>
-            <el-button @click="resetForm('loginForm')">重置</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div class="loginbox registerbox" v-else>
-        <el-form  id="register" class="login-form">
-          <el-form-item label="照片上传">
-            <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" drag :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </el-form-item>
-          <el-form-item label="姓名(中)" prop="username">
-            <el-input class="username" name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username"></el-input>
-          </el-form-item>
-           <el-form-item label="姓名(英)" prop="username">
-            <el-input class="username" name="username" type="text" v-model="loginForm.userename" autoComplete="on" placeholder="username"></el-input>
-          </el-form-item>
-           <el-form-item label="单位">
-            <el-input type="text"></el-input>
-          </el-form-item>
-          <el-form-item label="领域">
-            <el-cascader
-              :options="options2"
-              @active-item-change="handleItemChange"
-              :props="props"
-            ></el-cascader>
-          </el-form-item>
-          <el-form-item label="方向">
-            <el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input>
-            <el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input>
-          </el-form-item>
-          <el-form-item label="通讯邮箱">
-            <el-input type="text"></el-input>
-          </el-form-item>
-          <el-form-item label="个人网页链接">
-            <el-input type="text"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="password">
-            <el-input class="psw" name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"></el-input>
-          </el-form-item>
-          <el-form-item class="el-form-item-button">
-            <el-button type="primary" @click="switchPages()">登录界面</el-button>
-            <el-button type="primary" @click="register()">立即注册</el-button>
-            <el-button @click="resetForm('loginForm')">重置</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
+          <div class="loginbox" v-if="loginStatus">
+            <el-form  id="login" class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" status-icon label-width="80px" ref="loginForm" label-position="left">
+              <h3 class="title">vue-element-admin</h3>
+              <el-form-item label="账号" prop="username">
+                <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"></el-input>
+              </el-form-item>
+              <el-form-item class="el-form-item-button">
+                <el-button type="primary" @click.native.prevent="handleLogin" :loading="loading">立即登录</el-button>
+                <el-button type="primary" @click="switchPages()">注册界面</el-button>
+                <el-button @click="resetForm('loginForm')">重置</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div class="loginbox registerbox" v-else>
+            <el-form  id="register" class="login-form">
+              <el-form-item label="照片上传">
+                <el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/" drag :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
+              </el-form-item>
+              <el-form-item label="姓名(中)" prop="username">
+                <el-input class="username" name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username"></el-input>
+              </el-form-item>
+              <el-form-item label="姓名(英)" prop="username">
+                <el-input class="username" name="username" type="text" v-model="loginForm.userename" autoComplete="on" placeholder="username"></el-input>
+              </el-form-item>
+              <el-form-item label="单位">
+                <el-input type="text"></el-input>
+              </el-form-item>
+              <el-form-item label="领域">
+                <el-cascader
+                  :options="options2"
+                  @active-item-change="handleItemChange"
+                  :props="props"
+                ></el-cascader>
+              </el-form-item>
+              <el-form-item label="方向">
+                <el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input>
+                <el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input><el-input type="text" class="inputtext" placeholder="请输入关键词"></el-input>
+              </el-form-item>
+              <el-form-item label="通讯邮箱">
+                <el-input type="text"></el-input>
+              </el-form-item>
+              <el-form-item label="个人网页链接">
+                <el-input type="text"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input class="psw" name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"></el-input>
+              </el-form-item>
+              <el-form-item class="el-form-item-button">
+                <el-button type="primary" @click="switchPages()">登录界面</el-button>
+                <el-button type="primary" @click="register()">立即注册</el-button>
+                <el-button @click="resetForm('loginForm')">重置</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
+      
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+
 import { isValidUsername } from '@/utils/validate'
 
 export default {
@@ -104,6 +106,7 @@ export default {
           value: 'label',
           children: 'cities'
         },
+        
       loading: false,
       pwdType: 'password',
       loginStatus: true
@@ -161,16 +164,30 @@ export default {
 </script>
 
 <style lang="stylus">
+
 $bg = #2d3a4b
 $light_gray = #616161
 
 .login-container
+  background-image url('../../assets/snow.jpg')
+  background-repeat 'no-repeat'
+  background-position '25px auto'
+  background-size cover
+  width 100%
+  height auto
+  position absolute
+  
+  // background #50a3a2
+  // background -webkit-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%)
+  // background -moz-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%)
+  // background -o-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%)
+  // background linear-gradient(to bottom right, #50a3a2 0%, #53e3a6 100%)
   .el-input
     display inline-block
     height 47px
     width 85%
     input
-      background transparent
+      // background transparent
       border 0
       -webkit-appearance none
       border-radius 0
@@ -182,7 +199,7 @@ $light_gray = #616161
         -webkit-text-fill-color #fff!important
   .el-form-item
     border 1px solid rgba(255, 255, 255, 0.1)
-    background rgba(0, 0, 0, 0.1)
+    // background rgba(0, 0, 0, 0.1)
     border-radius 5px
     color #eee
 </style>
@@ -192,13 +209,16 @@ $bg = #d5def5
 $dark_gray = #889aa4
 $light_gray = #90aeff
 .login-container
-  height 100%
-  width 100%
-  background-color $bg
+  
+	
+	
+  //width 100%
+  //background-color $bg
   //background-image linear-gradient(45deg,#063053, #9951ff)
   .login-form
     // height 100%
-    position absolute
+    //position absolute
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 0.8) 0%, rgba(0,0,0,0.6) 100%);
     left 0
     right 0
     width 520px
@@ -235,5 +255,9 @@ $light_gray = #90aeff
       width 178px
       height 178px
       display block
+    
+      
+    
+      
 </style>
 
