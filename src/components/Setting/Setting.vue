80d1e237ab3d21bf0c901f9ac082a4a57eb4f4db
fxbabys <zhuguogen@qq.com>
2018-07-14 15:25:27 +0800

    add setting.vue

--- src/components/Setting/Setting.vue
+++ src/components/Setting/Setting.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="page">
-
+    <el-tabs tab-position="left">
+      <el-tab-pane label="修改密码">
+        <el-form label-position="right" label-width="100px">
+          <el-form-item label="旧密码">
+            <el-input type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="新密码">
+            <el-input type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">提交</el-button>
+            <el-button type="danger">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -18,4 +36,6 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+.el-input
+  width 200px
 </style>
