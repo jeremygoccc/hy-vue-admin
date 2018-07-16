4e30f22d80f6385b6af0d2eb3aed22e38b9e48bf
fxbabys <zhuguogen@qq.com>
2018-07-16 15:25:10 +0800

    modified the api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5b3f5c1f18892e144d79f955/hyVue"'
-});
+  BASE_API: '"https://yapi.homyit.cn/mock/25"'
+})
